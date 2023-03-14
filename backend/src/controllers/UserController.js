4ab81acefad272cf7a10c19101105ab9452c311e
Renanppcosta <renanppcosta@gmail.com>
2023-03-14 17:08:58 -0300

    CRUD of User completed

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -17,6 +17,76 @@ const createUser = async (req, res) =>{
     }
 }
 
+const getAllUsers = async (req,res) =>{
+    try {
+        const users = await prisma.user.findMany({
+            select:{
+                name: true,
+                email: true,
+                password: false,
+                photo_perfil: false,
+                createdAt: true
+
+            }
+        });
+        return res.json(users);
+    } catch (err) {
+        res.status(400).send({message: err.message});
+    }
+}
+
+const getUserById = async (req,res) =>{
+    try {
+        const user = await prisma.user.findUnique({
+            where:{
+                id: Number(req.params.id)
+            },
+            select:{
+                name: true,
+                email: true,
+                password: true,
+                photo_perfil: true,
+            }
+        })
+        return res.send(user);
+    } catch (err) {
+        res.status(400).send({message: err.message});
+    }
+}
+
+const editUser = async (req,res) =>{
+    try {
+        const data = req.body;
+        const user = await prisma.user.update({
+            where:{
+                id: Number(req.params.id)
+            },
+            data
+        })
+        return res.send(user);
+    } catch (err) {
+        res.status(400).send({message: err.message});
+    }
+}
+
+const removeUser = async (req,res) => {
+    try {
+        await prisma.user.delete({
+            where:{
+                id: Number(req.params.id)
+            }
+        })
+
+        return res.send("Usuário Deletado");
+    } catch (err) {
+        res.status(400).send({message: err.message});
+    }
+}
+
 module.exports = {
-    createUser
+    createUser,
+    getAllUsers,
+    getUserById,
+    editUser,
+    removeUser
 }
--- backend/src/router/UserRouter.js
+++ backend/src/router/UserRouter.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { createUser } = require("../controllers/UserController");
+const { createUser, getAllUsers, getUserById, editUser, removeUser } = require("../controllers/UserController");
 
 router.post("/", createUser);
+router.get("/", getAllUsers);
+router.get("/:id", getUserById);
+router.put("/:id", editUser);
+router.delete("/:id", removeUser);
 
 
 module.exports = router;

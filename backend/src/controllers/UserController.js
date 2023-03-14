5cda3d9290ac45e39b9ae6ffaaf331b0a2d9d763
Renanppcosta <renanppcosta@gmail.com>
2023-03-14 16:03:00 -0300

    create user completed

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -1,10 +1,12 @@
 const { prisma } = require("../prisma");
-const { userValidation } = require("../validations/UserValidation")
+const { userValidation } = require("../validations/UserValidation");
+const bcrypt = require("bcrypt");
 
 const createUser = async (req, res) =>{
     const data = req.body;
     try {
         await userValidation.validate(data);
+        req.body.password = bcrypt.hashSync(req.body.password, 10);
         const user = await prisma.user.create({
             data
         });

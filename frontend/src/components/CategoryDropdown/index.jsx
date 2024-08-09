4e821688e378ec9f13a2266878455902490e8bdb
Renanppcosta <renanppcosta@gmail.com>
2024-08-09 18:52:59 -0300

    feat: adding dropdown effect to categories and finalized stylized header

--- frontend/src/components/CategoryDropdown/index.jsx
+++ frontend/src/components/CategoryDropdown/index.jsx
@@ -13,7 +13,7 @@ export default function CategoryDropdown(){
         <>
             <div className="flex gap-2 items-center w-full cursor-pointer" onClick={showMenu}>
                 <a href="#" className="text-slate-950 hover:text-slate-700 hover:scale-110 duration-300">Categorias</a>
-                <i className="fa fa-chevron-down "></i>
+                <i className={`fa fa-chevron-down ${isOpen ? "rotate-180" : ""}`}></i>
             </div>
             {isOpen && (
                 <div className="bg-white w-full rounded-xl flex flex-col items-start p-4 font-bold gap-4 shadow-lg">
--- frontend/src/components/Navbar/index.jsx
+++ frontend/src/components/Navbar/index.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react"
 import logo from "../../assets/logo-todays-truth.png"
 import HamburguerMenu from "../HamburguerMenu"
 
 export default function Navbar(){
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setIsDropdownOpen(!isDropdownOpen);
+    }
+
     return(
         <>
             <header className="w-full h-24 flex justify-center items-center px-5 border-b shadow-md fixed top-0 z-10">
@@ -11,8 +18,21 @@ export default function Navbar(){
                     <nav className="hidden lg:flex gap-8">
                         <a href="#" className="text-slate-950 text-lg hover:text-slate-600 hover:scale-110 duration-300">Página Inicial</a>
                         <a href="#" className="text-slate-950 text-lg hover:text-slate-600 hover:scale-110 duration-300">Meu Perfil</a>
-                        <a href="#" className="text-slate-950 text-lg hover:text-slate-600 hover:scale-110 duration-300">Categorias</a>
-                        <a href="#" className="text-slate-950 text-lg hover:text-slate-600 hover:scale-110 duration-300">Entre em contato</a>
+                        <div className="relative" 
+                            onMouseEnter={toggleDropdown} 
+                            onMouseLeave={toggleDropdown}
+                        >
+                            <a href="#" className="text-slate-950 text-lg hover:text-slate-600 duration-300">Categorias</a>
+                            {isDropdownOpen &&(
+                                <div className="absolute bg-white w-48 rounded-xl flex flex-col items-start p-4 font-bold gap-4 shadow-lg">
+                                    <a href="#" className="text-slate-950 text-lg hover:text-slate-600 hover:scale-110 duration-300">Página Inicial</a>
+                                    <a href="#" className="text-slate-950 text-lg hover:text-slate-600 hover:scale-110 duration-300">Meu Perfil</a>
+                                    <a href="#" className="text-slate-950 text-lg hover:text-slate-600 hover:scale-110 duration-300">Página Inicial</a>
+                                    <a href="#" className="text-slate-950 text-lg hover:text-slate-600 hover:scale-110 duration-300">Meu Perfil</a>
+                                </div>
+                            )}
+                        </div>
+                        <a href="#" className="text-slate-950 text-lg hover:text-slate-00 hover:scale-110 duration-300">Entre em contato</a>
                     </nav>
                     <div className="hidden rounded-lg shadow-lg p-2 lg:flex items-center justify-center border">
                         <input type="text" className="outline-none w-full focus:w-[300px] duration-500" placeholder="Pesquisar Notícia"/>

fd210328974a6f84e0570d72c7a26a57e6794c4f
Renanppcosta <renanppcosta@gmail.com>
2024-08-20 20:09:38 -0300

    feat: scroll animation on cards created with gsap

--- frontend/src/components/Card/index.jsx
+++ frontend/src/components/Card/index.jsx
@@ -1,17 +1,54 @@
+import { useLayoutEffect, useRef } from "react";
+import { gsap } from "gsap";
 import "../Card/card.css"
-export default function Card({news}){
-    return(
-        <>
-            <div className="w-full flex flex-col items-center gap-4 p-4">
-                <article className="bg-white p-6 text-start mt-10 rounded-md shadow-2xl max-w-7xl md:flex md:h-64 md:gap-5 cursor-pointer">
-                    <img className="rounded-md w-full md:w-[30%]" src={news.banner} alt="Imagem da Noticia" />
-                    <div className="md:flex flex-col items-start">
-                        <h2 className="text-2xl font-bold text-slate-800 my-3">{news.title}</h2>
-                        <span className="text-xs uppercase text-slate-600">{news.date} | {news.category}</span>
-                        <p className="text-sm text-slate-600 md:text-lg md:mt-2">{news.text}</p>
-                    </div>
-                </article>
-            </div>
-        </>
-    )
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+export default function Card({ news }) {
+    const cardRef = useRef();
+    const tl = useRef();
+
+    useLayoutEffect(() => {
+        gsap.registerPlugin(ScrollTrigger);
+
+        const ctx = gsap.context(()=>{
+            tl.current = gsap.timeline({
+                scrollTrigger: {
+                    trigger: cardRef.current,
+                    start: "top 700px",
+                    end: "bottom 940px",
+                    scrub: 5,
+                }
+            }).to(cardRef.current, { x: 0, opacity: 1 });
+        }, cardRef) 
+
+        return () => {
+            gsap.killTweensOf(cardRef.current);
+        };
+    }, []);
+
+    return (
+        <div
+            className="card w-full flex flex-col items-center gap-4 p-4 translate-x-full opacity-0"
+            ref={cardRef}
+        >
+            <article className="bg-white p-6 text-start mt-10 rounded-md shadow-2xl max-w-7xl md:flex md:h-64 md:gap-5 cursor-pointer">
+                <img
+                    className="rounded-md w-full md:w-[30%]"
+                    src={news.banner}
+                    alt="Imagem da Noticia"
+                />
+                <div className="md:flex flex-col items-start">
+                    <h2 className="text-2xl font-bold text-slate-800 my-3">
+                        {news.title}
+                    </h2>
+                    <span className="text-xs uppercase text-slate-600">
+                        {news.date} | {news.category}
+                    </span>
+                    <p className="text-sm text-slate-600 md:text-lg md:mt-2">
+                        {news.text}
+                    </p>
+                </div>
+            </article>
+        </div>
+    );
 }

bea6dbc6a448d56e2e657a9f8d302211d818de70
Renanppcosta <renanppcosta@gmail.com>
2024-11-12 22:06:16 -0300

    feat: parameterized route created

--- frontend/src/pages/Search/index.jsx
+++ frontend/src/pages/Search/index.jsx
@@ -1,7 +1,10 @@
+import { useParams } from "react-router-dom"
+
 export function Search(){
+    const { title } = useParams();
     return(
         <>
-            <h1 className="text-3xl text-slate-950 mt-32 text-center">Notícias Encontradas</h1>
+            <h1 className="text-3xl text-slate-950 mt-32 text-center">Notícias Encontradas = {title}</h1>
         </>
     )
 }
